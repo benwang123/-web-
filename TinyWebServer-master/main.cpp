569bcda3dc76f9c3f2c4a407db126bef2bba6164
benwang123 <40230019+benwang123@users.noreply.github.com>
2021-04-11 22:16:46 +0800

    Add files via upload

--- TinyWebServer-master/main.cpp
+++ TinyWebServer-master/main.cpp
@@ -1,36 +1,24 @@
-#include "config.h"
+#include "webserver.h"
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    //需要修改的数据库信息,登录名,密码,库名
-    string user = "root";
-    string passwd = "root";
-    string databasename = "qgydb";
+    Arg_t arg;
+    //命令行解析,后续修改为利用配置参数进行传参
+    Config config("webserver.cfg");
 
-    //命令行解析
-    Config config;
-    config.parse_arg(argc, argv);
+    config.init(&arg);
 
     WebServer server;
 
     //初始化
-    server.init(config.PORT, user, passwd, databasename, config.LOGWrite, 
-                config.OPT_LINGER, config.TRIGMode,  config.sql_num,  config.thread_num, 
-                config.close_log, config.actor_model);
-    
+    server.init(&arg);
 
     //日志
     server.log_write();
-
-    //数据库
-    server.sql_pool();
-
-    //线程池
-    server.thread_pool();
-
-    //触发模式
-    server.trig_mode();
-
+   
     //监听
     server.eventListen();
 
