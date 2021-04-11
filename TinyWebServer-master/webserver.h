dde4a4f812f9c576d1697e6e3b0fdf5d9fc971c6
benwang123 <40230019+benwang123@users.noreply.github.com>
2021-04-11 22:13:46 +0800

    Add files via upload

--- TinyWebServer-master/webserver.h
+++ TinyWebServer-master/webserver.h
@@ -11,9 +11,14 @@
 #include <stdlib.h>
 #include <cassert>
 #include <sys/epoll.h>
+#include <unordered_map>
 
+//#include "./epoller/epoller.h"
 #include "./threadpool/threadpool.h"
-#include "./http/http_conn.h"
+#include "./memory/mempool.h"
+#include "./config.h"
+#include "./timer/timer_heap.h"
+#include "./util/util.h"
 
 const int MAX_FD = 65536;           //最大文件描述符
 const int MAX_EVENT_NUMBER = 10000; //最大事件数
@@ -25,49 +30,24 @@ public:
     WebServer();
     ~WebServer();
 
-    void init(int port , string user, string passWord, string databaseName,
-              int log_write , int opt_linger, int trigmode, int sql_num,
-              int thread_num, int close_log, int actor_model);
+    void init(Arg_t * arg);
 
-    void thread_pool();
-    void sql_pool();
     void log_write();
     void trig_mode();
     void eventListen();
     void eventLoop();
-    void timer(int connfd, struct sockaddr_in client_address);
-    void adjust_timer(util_timer *timer);
-    void deal_timer(util_timer *timer, int sockfd);
     bool dealclinetdata();
     bool dealwithsignal(bool& timeout, bool& stop_server);
     void dealwithread(int sockfd);
     void dealwithwrite(int sockfd);
+    int  setnonblocking(int fd);
 
-public:
+private:
     //基础
     int m_port;
     char *m_root;
     int m_log_write;
-    int m_close_log;
     int m_actormodel;
-
-    int m_pipefd[2];
-    int m_epollfd;
-    http_conn *users;
-
-    //数据库相关
-    connection_pool *m_connPool;
-    string m_user;         //登陆数据库用户名
-    string m_passWord;     //登陆数据库密码
-    string m_databaseName; //使用数据库名
-    int m_sql_num;
-
-    //线程池相关
-    threadpool<http_conn> *m_pool;
-    int m_thread_num;
-
-    //epoll_event相关
-    epoll_event events[MAX_EVENT_NUMBER];
 
     int m_listenfd;
     int m_OPT_LINGER;
@@ -75,8 +55,20 @@ public:
     int m_LISTENTrigmode;
     int m_CONNTrigmode;
 
-    //定时器相关
-    client_data *users_timer;
+    Epoll * epoller;
+    unordered_map<int, http_conn *> _users;
+    HeapTimer * timer;
+
+    void closeconn(http_conn * conn);
+
+    void Add(int a);
+
+public:
+
+    int m_pipefd[2]; 
+    int m_close_log;
+
     Utils utils;
 };
+
 #endif
