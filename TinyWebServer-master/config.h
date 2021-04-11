de675e05a9a7a8111677c1c5a4e1a59bdf9dcdb1
benwang123 <40230019+benwang123@users.noreply.github.com>
2021-04-11 22:01:38 +0800

    Add files via upload

--- TinyWebServer-master/config.h
+++ TinyWebServer-master/config.h
@@ -1,47 +1,52 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include "webserver.h"
+#include <fstream>
+#include <map>
 
 using namespace std;
 
+typedef struct arg
+{
+    int SERVER_PORT;     //端口号
+    int LOGWrite;        //日志写入方式
+    int TRIGMode;        //触发组合模式
+    int LISTENTrigmode;  //listenfd触发模式
+    int CONNTrigmode;    //connfd触发模式
+    int OPT_LINGER;      //优雅关闭链接
+
+    int MYSQL_NUM;       //mysql连接池数量
+    string MYSQL_ADDR;
+    string MYSQL_USER;
+    string MYSQL_PASSWD;
+    string MYSQL_DB;
+    int MYSQL_CLOSE_LOG;
+
+    int REDIS_NUM;       //redis连接池数量
+    string REDIS_ADDR;
+    int REDIS_PORT;
+    int REDIS_CLOSE_LOG;
+
+    int THREAD_NUM;      //线程池内的线程数量
+    int CLOSE_LOG;       //是否关闭日志
+    int ACTOR_MODE;      //并发模型选择
+
+}Arg_t;
+
 class Config
 {
+private:
+    string filename;
+    map<string, string> m_map;
+
 public:
-    Config();
+    Config(string file);
     ~Config(){};
-
-    void parse_arg(int argc, char*argv[]);
-
-    //端口号
-    int PORT;
-
-    //日志写入方式
-    int LOGWrite;
-
-    //触发组合模式
-    int TRIGMode;
-
-    //listenfd触发模式
-    int LISTENTrigmode;
-
-    //connfd触发模式
-    int CONNTrigmode;
-
-    //优雅关闭链接
-    int OPT_LINGER;
-
-    //数据库连接池数量
-    int sql_num;
-
-    //线程池内的线程数量
-    int thread_num;
-
-    //是否关闭日志
-    int close_log;
-
-    //并发模型选择
-    int actor_model;
+    bool ReadConfigFile();
+    void RemoveSpace(string &str);
+    void parsearg(Arg_t * arg);
+    void init(Arg_t * _arg);
 };
+
 
 #endif
